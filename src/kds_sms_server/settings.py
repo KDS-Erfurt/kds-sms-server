fb87ae21f702936d38955cb6fd64416c3f77f186
Julius Koenig <julius@koenig-site.de>
2025-06-04 15:19:24 +0200

    Update `settings.py` to integrate `SqlalchemySettings` and enhance field validation
    Replaced `UvicornServerSettings` with `SqlalchemySettings` in the `Settings` class for database integration. Added constraints to SMS-related fields (`sms_data_max_size`, `sms_number_max_size`, `sms_message_max_size`) for improved validation. Introduced new logging settings to ignore specific loggers by exact matches or patterns.

--- src/kds_sms_server/settings.py
+++ src/kds_sms_server/settings.py
@@ -1,9 +1,8 @@
-from ipaddress import IPv4Address, IPv4Network
 from pathlib import Path
 
 from pydantic import Field
 from pydantic_settings import BaseSettings
-from wiederverwendbar.uvicorn import UvicornServerSettings
+from wiederverwendbar.sqlalchemy import SqlalchemySettings
 
 from kds_sms_server.gateways import AVAILABLE_GATEWAY_CONFIGS
 from wiederverwendbar.logger import LoggerSettings
@@ -12,7 +11,7 @@ from wiederverwendbar.pydantic import FileConfig
 from kds_sms_server.server import AVAILABLE_SERVER_CONFIGS
 
 
-class Settings(FileConfig, BaseSettings, LoggerSettings, UvicornServerSettings):
+class Settings(FileConfig, BaseSettings, LoggerSettings, SqlalchemySettings):
     model_config = {
         "env_prefix": "KDS_SMS_SERVER_",
         "case_sensitive": False
@@ -25,6 +24,12 @@ class Settings(FileConfig, BaseSettings, LoggerSettings, UvicornServerSettings):
     log_console_format: str = Field(default="%(message)s",
                                     title="Console Log Format",
                                     description="The log format for the console")
+    log_ignored_loggers_equal: list[str] = Field(default_factory=list,
+                                                 title="Ignored Loggers Equal",
+                                                 description="Loggers that are equal to these will be ignored.")
+    log_ignored_loggers_like: list[str] = Field(default_factory=lambda: ["sqlalchemy", "pymysql", "asyncio", "parso", "engineio", "socketio", "python_multipart.multipart"],
+                                                title="Ignored Loggers Like",
+                                                description="Loggers that contain these will be ignored.")
 
     # sms settings
     sms_number_allowed_chars: str = Field(default="+*#()0123456789 ", title="Allowed Number Characters",
@@ -33,9 +38,10 @@ class Settings(FileConfig, BaseSettings, LoggerSettings, UvicornServerSettings):
                                           description="Replace Number Characters.")  # ToDo: CHeck if used in tcp_server and api_server
     sms_replace_zero_numbers: str | None = Field(default=None, title="Replace Zero Numbers",
                                                  description="Replace all zero numbers with this string.")  # ToDo: CHeck if used in tcp_server and api_server
-    sms_data_max_size: int = Field(default=2048, title="Max Data Size", description="Max Data Size for SMS.")  # ToDo: CHeck if used in tcp_server and api_server
-    sms_number_max_size: int = Field(default=20, title="Max Number Size", description="Max Number Size for SMS.")  # ToDo: CHeck if used in tcp_server and api_server
-    sms_message_max_size: int = Field(default=1600, title="Max Message Size", description="Max Message Size for SMS.")  # ToDo: CHeck if used in tcp_server and api_server
+    sms_data_max_size: int = Field(default=2048, title="Max Data Size", description="Max Data Size for SMS.", ge=1024)  # ToDo: CHeck if used in tcp_server and api_server
+    sms_number_max_size: int = Field(default=20, title="Max Number Size", description="Max Number Size for SMS.", ge=1, le=50)  # ToDo: CHeck if used in tcp_server and api_server
+    sms_message_max_size: int = Field(default=1600, title="Max Message Size", description="Max Message Size for SMS.", ge=1,
+                                      le=1600)  # ToDo: CHeck if used in tcp_server and api_server
     sms_logging: bool = Field(default=False, title="SMS Logging", description="Enable SMS Logging content logging.")  # ToDo: CHeck if used in tcp_server and api_server
     sms_server: dict[str, AVAILABLE_SERVER_CONFIGS] = Field(default_factory=dict, title="Server",
                                                             description="Server configuration.")  # ToDo: CHeck if used in tcp_server and api_server
