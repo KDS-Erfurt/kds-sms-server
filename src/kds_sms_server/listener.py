a7e1515559658a46e0550bb389db4a9ec59ece7e
Julius Koenig <julius@koenig-site.de>
2025-06-04 21:21:34 +0200

    Refactor SMS processing: move gateway initialization from `listener.py` to `worker.py`, enhance SMS handling logic and maintain modular design consistency.

--- src/kds_sms_server/listener.py
+++ src/kds_sms_server/listener.py
@@ -3,13 +3,10 @@ import time
 
 from wiederverwendbar.logger import LoggerSingleton
 
-from kds_sms_server.gateways.gateway import BaseGateway
-from kds_sms_server.gateways.teltonika.gateway import TeltonikaGateway
-from kds_sms_server.gateways.vonage.gateway import VonageGateway
+from kds_sms_server.settings import settings
 from kds_sms_server.server.server import BaseServer
 from kds_sms_server.server.api.server import ApiServer
 from kds_sms_server.server.tcp.server import TcpServer
-from kds_sms_server.settings import settings
 
 # noinspection PyArgumentList
 logger = LoggerSingleton(name=__name__,
@@ -22,7 +19,7 @@ class SmsListener:
     def __init__(self):
         logger.info(f"Initializing SMS-Listener ...")
 
-        # initialize servers and gateways
+        # initialize servers
         logger.info("Initializing servers ...")
         self._server: list[BaseServer] = []
         for server_config_name, server_config in settings.sms_server.items():
@@ -46,31 +43,6 @@ class SmsListener:
             logger.error(f"No servers are configured. Please check your settings.")
             sys.exit(1)
         logger.debug("Initializing servers ... done")
-
-        logger.info("Initializing gateways ...")
-        # self._next_sms_gateway_index: int | None = None
-        self._gateways: list[BaseGateway] = []
-        for gateway_config_name, gateway_config in settings.sms_gateways.items():
-            if len(gateway_config_name) > 20:
-                logger.error(f"Gateway name '{gateway_config_name}' is too long. Max size is 20 characters.")
-                sys.exit(1)
-
-            gateway_cls = None
-            if gateway_config.type == "teltonika":
-                gateway_cls = TeltonikaGateway
-            elif gateway_config.type == "vonage":
-                gateway_cls = VonageGateway
-
-            if gateway_cls is None:
-                logger.error(f"Unknown gateway type '{gateway_config.type}'.")
-                sys.exit(1)
-
-            gateway = gateway_cls(name=gateway_config_name, config=gateway_config)
-            self._gateways.append(gateway)
-        if len(self._gateways) == 0:
-            logger.error(f"No gateways are configured. Please check your settings.")
-            sys.exit(1)
-        logger.debug("Initializing gateways ... done")
 
         logger.debug(f"Initializing SMS-Listener ... done")
 
--- src/kds_sms_server/worker.py
+++ src/kds_sms_server/worker.py
@@ -1,10 +1,13 @@
-import time
+import sys
 
 from wiederverwendbar.logger import LoggerSingleton
 from wiederverwendbar.task_manger import TaskManager, Task, EverySeconds
 
-from kds_sms_server.console import console
+from kds_sms_server.db import Sms, SmsStatus
 from kds_sms_server.settings import settings
+from kds_sms_server.gateways.gateway import BaseGateway
+from kds_sms_server.gateways.teltonika.gateway import TeltonikaGateway
+from kds_sms_server.gateways.vonage.gateway import VonageGateway
 
 # noinspection PyArgumentList
 logger = LoggerSingleton(name=__name__,
@@ -16,7 +19,33 @@ logger = LoggerSingleton(name=__name__,
 class SmsWorker(TaskManager):
     def __init__(self):
         logger.info(f"Initializing SMS-Worker ...")
-        super().__init__(worker_count=settings.sms_background_worker_count, daemon=True, logger=logger)
+        super().__init__(name="SMS-Worker", worker_count=settings.sms_background_worker_count, daemon=True, logger=logger)
+
+        # initialize gateway
+        logger.info("Initializing gateways ...")
+        self._next_sms_gateway_index: int | None = None
+        self._gateways: list[BaseGateway] = []
+        for gateway_config_name, gateway_config in settings.sms_gateways.items():
+            if len(gateway_config_name) > 20:
+                logger.error(f"Gateway name '{gateway_config_name}' is too long. Max size is 20 characters.")
+                sys.exit(1)
+
+            gateway_cls = None
+            if gateway_config.type == "teltonika":
+                gateway_cls = TeltonikaGateway
+            elif gateway_config.type == "vonage":
+                gateway_cls = VonageGateway
+
+            if gateway_cls is None:
+                logger.error(f"Unknown gateway type '{gateway_config.type}'.")
+                sys.exit(1)
+
+            gateway = gateway_cls(name=gateway_config_name, config=gateway_config)
+            self._gateways.append(gateway)
+        if len(self._gateways) == 0:
+            logger.error(f"No gateways are configured. Please check your settings.")
+            sys.exit(1)
+        logger.debug("Initializing gateways ... done")
 
         # create tasks
         logger.info("Initializing tasks ...")
@@ -33,69 +62,50 @@ class SmsWorker(TaskManager):
 
         logger.debug(f"Starting SMS-Worker ... done")
 
-    # @property
-    # def gateways(self) -> tuple[BaseGateway, ...]:
-    #     with self.lock:
-    #         if self._next_sms_gateway_index is None:
-    #             self._next_sms_gateway_index = 0
-    #         else:
-    #             self._next_sms_gateway_index += 1
-    #         if self._next_sms_gateway_index >= len(self._gateways):
-    #             self._next_sms_gateway_index = 0
-    #
-    #         gateways = []
-    #         for gateway in self._gateways[self._next_sms_gateway_index:]:
-    #             gateways.append(gateway)
-    #         if self._next_sms_gateway_index > 0:
-    #             for gateway in self._gateways[:self._next_sms_gateway_index]:
-    #                 gateways.append(gateway)
-    #     return tuple(gateways)
-
-    # def handle_sms(self, server: TcpServer | ApiServer, number: str, message: str) -> tuple[bool, str]:
-    #     logger.debug(f"{server} - Processing SMS ...")
-    #
-    #     # check number
-    #     if len(number) > self.settings.sms_number_max_size:
-    #         return False, f"Received number is too long. Max size is '{self.settings.sms_number_max_size}'.\nnumber_size={len(number)}"
-    #     _number = ""
-    #     for char in number:
-    #         if char not in list(self.settings.sms_number_allowed_chars):
-    #             return False, f"Received number contains invalid characters. Allowed characters are '{self.settings.sms_number_allowed_chars}'.\nnumber='{number}'"
-    #         if char in list(self.settings.sms_number_replace_chars):
-    #             char = ""
-    #         _number += char
-    #     number = _number
-    #     del _number
-    #
-    #     # replace zoro number
-    #     if self.settings.sms_replace_zero_numbers is not None:
-    #         if number.startswith("0"):
-    #             number = self.settings.sms_replace_zero_numbers + number[1:]
-    #
-    #     # check a message
-    #     if len(message) > self.settings.sms_message_max_size:
-    #         return False, f"Received message is too long. Max size is '{self.settings.sms_message_max_size}'.\nmessage_size={len(message)}"
-    #
-    #     # log sms received successfully
-    #     if self.settings.sms_logging:
-    #         logger.debug(f"{server} - Processing SMS ... done\nnumber={number}\nmessage='{message}'")
-    #     else:
-    #         logger.debug(f"{server} - Processing SMS ... done\nnumber={number}")
-    #
-    #     # send sms to gateways
-    #     for gateway in self.gateways:
-    #         # check if gateway is available
-    #         if not gateway.check():
-    #             continue
-    #
-    #         # send it with gateway
-    #         result = gateway.send_sms(number, message)
-    #         if result:
-    #             return True, ""
-    #     return False, f"Error while sending SMS. Not gateways left."
-
     def handle_sms(self):
-        logger.debug("Handle SMS ...")
+        # get all queued sms
+        queued_sms = Sms.get_all(status=SmsStatus.QUEUED, order_by=Sms.received_datetime)
+        if len(queued_sms) == 0:
+            return
+        queued_sms_count = len(queued_sms)
+        logger.debug(f"Processing {queued_sms_count} queued SMS ...")
+
+        # calculate next_sms_gateway_index
+        if self._next_sms_gateway_index is None:
+            self._next_sms_gateway_index = 0
+        else:
+            self._next_sms_gateway_index += 1
+        if self._next_sms_gateway_index >= len(self._gateways):
+            self._next_sms_gateway_index = 0
+
+        # order gateways
+        gateways = []
+        for gateway in self._gateways[self._next_sms_gateway_index:]:
+            gateways.append(gateway)
+        if self._next_sms_gateway_index > 0:
+            for gateway in self._gateways[:self._next_sms_gateway_index]:
+                gateways.append(gateway)
+
+        # send sms with gateways
+        logger.debug("------------------------------------------------------------------------------------------------------")
+        result = False
+        message = ""
+        for gateway in gateways:
+            # check if gateway is available
+            if not gateway.check():
+                continue
+
+            # send it with gateway
+            result = gateway.send_sms(number, message)
+            if result:
+                result = True
+                message = f"SMS sent successfully via {gateway}."
+                return True, ""
+        logger.debug("------------------------------------------------------------------------------------------------------")
+        return False, f"Error while sending SMS. Not gateways left."
+
+        print()
+        ...
 
     def cleanup_sms(self):
-        logger.debug("Handle SMS ...")
+        ...
